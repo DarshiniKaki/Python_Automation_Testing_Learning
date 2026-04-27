6405b4bed746f7aa5fceabc2e397f53a88464bcd
DarshiniKaki <darshini.kaki@covalensedigital.net>
2026-04-27 23:16:16 +0530

    Added Assertions with POM for website login test

--- tests/test_website_login.py
+++ tests/test_website_login.py
@@ -1,23 +1,18 @@
 from playwright.sync_api import sync_playwright
 from pages.login_page import LoginPage
-from data.login_data import test_data
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=500)
-    page = browser.new_page()
 
-    page.goto("https://the-internet.herokuapp.com/login")
-    login_page = LoginPage(page)
-
-    for data in test_data:
-        print("Testing:", data["username"])
-
-        login_page.enter_username(data["username"])
-        login_page.enter_password(data["password"])
-        login_page.click_login()
-
-        page.wait_for_timeout(3000)
+def test_login_success():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
         page.goto("https://the-internet.herokuapp.com/login")
+        login = LoginPage(page)
+        login.enter_username("tomsmith")
+        login.enter_password("SuperSecretPassword!")
+        login.click_login()
 
-    browser.close()
+        assert "Secure Area" in page.content()
+
+        browser.close()
