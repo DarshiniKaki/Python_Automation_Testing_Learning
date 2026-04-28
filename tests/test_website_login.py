07449cca97de03335e2ae0a22875e0f8b67bbb85
DarshiniKaki <darshini.kaki@covalensedigital.net>
2026-04-28 13:02:24 +0530

    Modified with failure message negative case

--- tests/test_website_login.py
+++ tests/test_website_login.py
@@ -2,17 +2,17 @@ from utils.browser_setup import launch_browser
 from pages.login_page import LoginPage
 
 
-def test_login_success():
+def test_login_failure():
     p, browser, page = launch_browser()
 
     page.goto("https://the-internet.herokuapp.com/login")
 
     login = LoginPage(page)
-    login.enter_username("tomsmith")
-    login.enter_password("SuperSecretPassword!")
+    login.enter_username("wrong")
+    login.enter_password("wrong")
     login.click_login()
 
-    assert "Secure Area" in page.content()
+    assert "invalid" in page.content()
 
     browser.close()
     p.stop()
